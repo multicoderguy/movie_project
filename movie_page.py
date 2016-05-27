8b105e894f6d212f0f605571496c5e28347ec991
Dan Wimpelberg <DWIMPELB@cablevision.com>
2016-05-27 11:05:17 -0400

    Initial creation and checkin of code

---
+++ movie_page.py
@@ -0,0 +1,37 @@
+import media
+import fresh_tomatoes
+
+""" create array of movies """
+movie_array = [
+    ["Freddy Got Fingered",
+        "Gordy, and animator, leaves home to fulfill his dreams.",
+        "http://images.moviepostershop.com//freddy-got-fingered-movie-poster-1020476187.jpg",
+        "http://www.youtube.com/watch?v=OwoVgMEXusM"],
+    ["Quest for the Hold Grail",
+        "King Arthur searches for the Holy Grail.",
+        "http://www.movie-list.com/img/posters/big/zoom/montypythonandtheholygrail.jpg",
+        "https://www.youtube.com/watch?v=LG1PlkURjxE"],
+    ["Austin Powers: International Man of Mystery",
+        "The international man of mystery fights the ultimate Dr. Evil",
+        "http://static.rogerebert.com/uploads/movie/movie_poster/austin-powers-international-man-of-mystery-1997/large_mLVymsqdkwNNKjw2ZXn8fkor3om.jpg",
+        "http://www.youtube.com/watch?v=_tQMdNoc8T8"],
+    ["War Party",
+         "Battle of Milk River re-enactment goes wrong.",
+         "http://images.moviepostershop.com/war-party-movie-poster-1989-1020210795.jpg",
+         "http://www.youtube.com/watch?v=sgXlr5_KRBI"],
+    ["Convoy",
+         "Rubby Ducky gets mad and fights back.",
+         "http://www.impawards.com/1978/posters/convoy_ver2.jpg",
+         "https://www.youtube.com/watch?v=sA0OP_x_zLc&feature=youtu.be"]
+        
+    ]
+
+""" initialize array to store movei objects """
+movie_list = []
+
+""" iterate over movie list array and generate a set of Movie objects """
+for movie_detail in movie_array:
+    movie_list.append(media.Movie(movie_detail[0],movie_detail[1],movie_detail[2],movie_detail[3]))
+
+""" call prewritten method to render list of Movie objects """
+fresh_tomatoes.open_movies_page(movie_list)
